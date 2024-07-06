6f79828fefc2519732fe327b106da3e40cdc4908
Ulrik Moe <ulrik.moe@gmail.com>
2024-07-06 15:06:38 +0200

    Update

--- index.js
+++ index.js
@@ -1,100 +1,157 @@
-const fs = require('fs');
-const cache = {};
+'use strict';
+
+const { Transform } = require('node:stream');
+const Path = require('node:path');
+const fs = require('node:fs');
 const inclRegex = /{% include "(.+?)" %}/g;
 const varRegex = /{{ (.+?) }}/g;
-
-
-// Set cache manually
-function setCache(filename, str, mtime=false) {
-    cache[filename] = {
-        str,
-        mtime: mtime || Date.now()
-    };
-}
+const uri = 'src/'; // TODO: expose this as an option
+const cache = new Map();
 
 function getFile(filename) {
-    try {
-        // Modification time (POSIX)
-        const mtime = fs.statSync(filename).mtimeMs / 1000;
-
-        // Serve from cache if possible
-        if (cache[filename] && mtime <= cache[filename].mtime) {
-            return cache[filename];
-        }
-        return cache[filename] = { str: fs.readFileSync(filename, 'utf8'), mtime };
-    } catch (e) {
-        throw new Error(filename + ' does not exist');
+    const stat = fs.statSync(uri + filename, { throwIfNoEntry: false });
+    if (!stat) {
+        console.error('File not found: ' + filename);
+        throw 'File not found: ' + filename;
     }
+    let obj = cache.get(filename);
+    if (obj && obj.mtime === stat.mtimeMs) return obj;
+    obj = {
+        str: fs.readFileSync(uri + filename, 'utf8'),
+        mtime: stat.mtimeMs,
+    };
+    cache.set(filename, obj);
+    return obj;
 }
 
-// Recursive include files
-function includer(str, mainFile=false) {
-    return str.replace(inclRegex, (m, path) => {
-        const file = getFile(path)
-        if (mainFile && file.mtime * 1000 > mainFile.stat.mtimeMs) {
-            mainFile.stat.mtimeMs = file.mtime * 1000;
-            mainFile.stat.mtime = file.mtime;
+// Recursively replace inclRegex with the content of the included file
+function includer(templateString, mainFile = false) {
+    if (typeof templateString !== 'string') {
+        throw new TypeError('Expected the first argument to be a string');
+    }
+
+    return templateString.replace(inclRegex, (match, filePath) => {
+        try {
+            const file = getFile(filePath);
+            if (!file) {
+                console.warn(`File not found: ${filePath}`);
+                return match; // Return the original match if the file is not found
+            }
+
+            // Update the mtime of the main file if necessary
+            if (mainFile && file.mtime > mainFile.stat.mtimeMs) {
+                mainFile.stat.mtimeMs = file.mtime;
+            }
+
+            // Recursively process included files
+            return includer(file.str, mainFile);
+        } catch (error) {
+            console.error(`Error including file: ${filePath}`, error);
+            return ''; // Return an empty string or handle the error as appropriate
         }
-        return includer(file.str, mainFile);
     });
 }
 
-// Flatten objects
-function flatten (arr, opts={}) {
-    const prefix = opts.prefix || '.';
-    const suffix = opts.suffix || '';
-    const output = {};
+function replaceVariables(template, variables) {
+    if (typeof variables !== 'object' || variables === null) {
+        console.log('Variables must be an object');
+        return template;
+    }
+    return template.replace(varRegex, (match, variableName) => {
+        const value = variables[variableName];
+        if (value === null || value === undefined) {
+            console.error('Variable not found:', variableName);
+            return match;
+        }
+        return String(value);
+    });
+}
 
-    function step (obj, prev) {
-        const keys = Object.keys(obj);
-        for (let i = 0; i < keys.length; i++) {
-            const val = obj[keys[i]];
-            const name = (prev) ? prev + prefix + keys[i] + suffix : keys[i];
-            if (val && (val.constructor === Object || val.constructor === Array)) {
-                step(val, name);
-            } else {
-                output[name] = val;
+/**
+ * Processes a template string by including external content and replacing variables.
+ */
+function fromString(str, vars, opts = {}) {
+    if (typeof str !== 'string') {
+        throw new TypeError('Expected a string for the first argument.');
+    }
+    if (typeof vars !== 'object' || vars === null) {
+        throw new TypeError('Expected an object for the second argument.');
+    }
+
+    try {
+        const fullStr = includer(str);
+        return replaceVariables(fullStr, vars, opts);
+    } catch (error) {
+        console.error('Error processing the string:', error);
+        return ''; // Return an empty string or handle the error as appropriate.
+    }
+}
+
+function parseFile(file) {
+    const str = file.contents.toString();
+    const obj = { str, path: file.path.substring(file._base.length + 1) };
+    const lines = str.split('\n', 10); // TODO: make this more efficient and stop...
+    if (lines[0] === '<!--') {
+        for (let i = 1; i < lines.length; i++) {
+            const split = lines[i].split(':');
+            if (!split[1]) break;
+            obj[split[0]] = split[1].trim();
+        }
+    }
+    if (!obj.title) throw 'No title found in file: ' + file.path;
+    if (!obj.url) throw 'No URL found in file: ' + file.path;
+    return obj;
+}
+
+function flattenObject(obj, parentKey) {
+    let result = {};
+    Object.entries(obj).forEach(([key, value]) => {
+        const _key = parentKey ? `${parentKey}[${key}]` : key;
+        if (typeof value === 'object') {
+            Object.assign(result, flattenObject(value, _key));
+        } else {
+            result[_key] = value;
+        }
+    });
+    return result;
+}
+
+function tap(fn) {
+    return new Transform({
+        objectMode: true,
+        async transform(file, enc, cb) {
+            try {
+                await fn(file);
+                cb(null, file);
+            } catch (err) {
+                cb(err);
+            }
+        },
+    });
+}
+
+function envParser(o) {
+    const args = process.argv.slice(2);
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+        if (arg.startsWith('--')) {
+            const key = arg.slice(2);
+            const nextArg = args[i + 1];
+            if (nextArg && !nextArg.startsWith('--')) {
+                o[key] = nextArg;
+                i++;
             }
         }
     }
-
-    for (let i = 0; i < arr.length; i++) {
-        step(arr[i], null);
-    }
-    return output;
+    return o;
 }
 
-function replaceVariables(str, vars, opts) {
-    if (typeof vars !== 'object') { return str; }
-    const regex = opts.varRegex ? opts.varRegex : varRegex;
-
-    const ret = str.replace(regex, (match, k1) => {
-        if (typeof vars[k1] === 'string') {
-            // Once more to replace vars in vars[k1].
-            return vars[k1].replace(regex, (m2, k2) => vars[k2] || m2);
-        }
-        if (typeof vars[k1] === 'number') {
-            return vars[k1];
-        }
-        return match;
+function writeSourceMap(dest, str) {
+    const dir = Path.dirname(dest);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    fs.writeFile(dest + '.map', str, (err) => {
+        if (err) console.error(err);
     });
-    return ret;
 }
 
-function fromString(str, vars, opts={}) {
-    return replaceVariables(includer(str), vars, opts);
-}
-
-function render(file, vars, opts={}) {
-    const str = file.contents.toString('utf8'); // maybe consider utf16
-    cache[file.path.substring(file._cwd + 1)] = {
-        str,
-        mtime: file.stat.mtimeMs / 1000
-    }
-    file.contents = Buffer.from(
-        replaceVariables(includer(str, file), vars, opts)
-    );
-    return file;
-}
-
-module.exports = () => ({ render, fromString, setCache, getFile, flatten });
+module.exports = () => ({ parseFile, fromString, flattenObject, tap, writeSourceMap, envParser });
